822f112a8f7a24293418239c9cb7b540e6d7fcc3
Erik Olsen <erikonj@gmail.com>
2022-02-09 13:07:56 -0500

    Removed all of the pointless sudos

--- installYuzu.sh
+++ installYuzu.sh
@@ -1,16 +1,16 @@
 echo Installing latest YUZU canary build...
 
-sudo rm -rf yuzu-canary
+rm -rf yuzu-canary
 
-sudo git clone --recursive https://github.com/yuzu-emu/yuzu-canary.git
+git clone --recursive https://github.com/yuzu-emu/yuzu-canary.git
 
 cd yuzu-canary
 
 
-sudo mkdir build && cd build
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release
-sudo make
-sudo make install
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+make install
 
 echo Install Complete!
 
